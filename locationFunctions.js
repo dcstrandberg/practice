72fa28fe55616472355a8ad8f329d2e5bf489107
dcs <dcstrandberg@gmail.com>
2016-04-21 15:48:48 -0500

    Renamed locationDisplay.js because it had a stupid typo in its name. Updated locationDisplay HTML page so that I could display the different user objects and their distances. Added a distance property to the newUser class so that we can save the distance for each user. It's not a permanant solution since the distance property for any given user will be modified for each user they're close to. Need to find a way to create a clone of the objects. Currently the part that's not working is returning the location object from getLocation() to the getDistance() function. I.e. the coords property isn't available. I'm pretty sure I dealt with this in the earlier locationTests files. I'll investigate.

--- locationFunctions.js
+++ locationFunctions.js
@@ -1,34 +1,34 @@
 /*This document is simply to try to get the distance function and all related functions working properly. Building the closeList should only require the functions contained herein. */
 
-var geoInfo, i=0, j=0, userList=[];
+var geoInfo, i=0, j=0, userList=[], htmlText = "";
 
 /*First we'll generate a list of 100 users*/
 for (i=0; i<100; i++) {
-    userList[i] = newUser(i);
+    userList[i] = new newUser(i);
 }
-//Then we'll check location for the first user against all the rest
 
+//Then in the HTML we'll check location for the first user against all the rest
 
 
 /*Include a stripped down user class that will contain ID and location*/
 function newUser(ID) {
     this.ID = ID;
+    this.distance;
     //Generate random maxDistance between .5 and 2.5 in .5 steps
     this.maxDistance = ( ( Math.floor( (Math.random() * 5 ) ) / 2) + 0.5);
-    this.location = []; //Eventually we'll want to make location private somehow
-    //this.lastActive = new Date;
-    
+    this.location = undefined; //Eventually we'll want to make location private somehow -- Not really used at all...
     this.blockList = []; //Add blockList of UIDs so we can check if the close user is blocked
     this.closeList = []; //Keep closeList because we'll generate it at the end
 
-    // Have to add in methods for updating location
+    // Have to add in methods for periodically updating location/closeList
     //LOCATION SHOULDN'T BE DIRECTLY ACCESSIBLE. IT SHOULD BE ENCASED IN A FUNCTION BUT HOW DO YOU DO THAT WITH TWO USERS.
     //For now we'll just have it be a function
     this.showLocation = function(loc) {
         //return loc; //Returns a location object for passing to distance function
         /*document.getElementById("demo").innerHTML = 
             "Location: " + loc.coords.latitude + ", " + loc.coords.longitude;*/
-        geoInfo = loc;
+        
+        this.location = loc; //Save the location object to this.location
         return loc; //This should get getLocation to return the location object
     };
     this.getLocation = function() {
@@ -37,13 +37,13 @@ function newUser(ID) {
         } else {
             /*document.getElementById("demo").innerHTML =  
                 "<p>Geolocation is not supported by this browser.</p>";*/
-                console.log("Location information not available");
+                document.getElementById("error").innerHTML = "Location information not available";
         }
     return;
     };
     
-    this.distance = function(user1) {
-        var distance, lat0, long0, lat1, long1, earthRadius = 6371000; //Meters
+    this.getDistance = function(user1) {
+        var dist, lat0, long0, lat1, long1, earthRadius = 6371000; //Meters
         //Get the location coordinates for each user
         lat0 = this.getLocation().coords.latitude;
         long0 = this.getLocation().coords.longitude;
@@ -60,36 +60,45 @@ function newUser(ID) {
                 Math.cos(lat0) * Math.cos(lat1) *
                 Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var distance = earthRadius * c;
+        var dist = earthRadius * c;
         
-        return distance;
-    }
-    this.genCloseList() = function () {
+        return dist;
+    };
+    this.genCloseList = function () {
         //First clear closeList of previous value
         this.closeList = [];
+        var currentDist = undefined;
         //For every user in the "database"
         for (i=0; i < userList.length; i++) { 
         //in the future we'll check a database for the list of users instead.
         
             //If the user is close enough and NOT the user who's looking...
-            if ( this.distance(userList[i]) <= this.maxDistance && 
-                    this !== userList[i] ) {        
+            currentDist = this.getDistance(userList[i]); 
+            if ( currentDist <= this.maxDistance && this !== userList[i] ) {
 
                 //If the close User doesn't match the ID of a blocked user...
                 if ( this.blockList.indexOf( userList[i].ID ) < 0 ) {
                 
                     //THEN we can add the current user to closeList
                     this.closeList.push(userList[i]); 
+                    userList[i].distance = currentDist;
                 }
             }
         }
         //Once all the users are added to closeList, sort it with closest first
         this.closeList.sort( function(a,b) {return a-b} ); 
         //IF WE WANNA SORT IT BY USERS THAT HAVE POSITIVE RATINGS, THIS IS WHERE WE'D DO THAT. OR PERHAPS IN ANOTHER FUNCTION THAT GETS CALLED HERE
+        
+        //Temporarily add HTML display code
+        for (i=0; i < this.closeList.length; i++) {
+            htmlText += "<p id='close" + i + "'>User #" + closeList[i].ID + 
+            ": " + closeList[i].distance.toFixed(1) + " km away</p>";
+        }
+        
         return;
-    }
-    this.sortCloseList() = function() {
+    };
+    this.sortCloseList = function() {
         //If users have a positive rating, put them at the top of the list
-    }    
+    };    
 }    
 
