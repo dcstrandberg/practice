6cc43c21f84de4184c8530581516af151e5cb8f5
dcs <dcstrandberg@gmail.com>
2016-04-25 11:01:14 -0500

    Changed the overflow settings for the #closeuser list, so that the horizontal scroll bar doesn't show up when the element gets slid off to the right. Removed the margins as well from each closeUser element. Also changed the hover dropshadow effect to a hover background-color change effect in the jQuery file.

--- jquery_animations.js
+++ jquery_animations.js
@@ -9,21 +9,25 @@ $(document).ready(function() { //Only execute once the document's loaded
     function closeAnim() {
     //$("[id ^= 'closeuser']").css('border','1px solid green');
     $("[id ^= 'closeuser']").hover(function() {
-        $(this).css('box-shadow','5px 5px 10px #777777');
+        $(this).css('background-color','#f08a6f');
     }, function() {
-        $(this).css('box-shadow','0 0 0');    
+        $(this).css('background-color','#efefef');    
     });
     $("[id ^= 'closeuser']").click(function() {
         //set position to relative so that we can move it
-        $(this).css('position','relative');
+        $(this).css({
+            'position':'relative',
+            'overflow':'hidden'
+        });
         $(this).animate({
-            left: '100%',
+            left: '150%',
             opacity: 0,
-            width: 'hide'            
-        }, 500/*, function() {
+            width: 'hide',
+            height: 'hide'            
+        }, 500, function() {
            //After animating the slide right and hiding the element, remove the <p> element from the HTML page. We still need to figure out how to remove the user from closeList and/or add a user to matchList
            $(this).remove(); 
-        }*/);
+        });
     });
     }
 });
